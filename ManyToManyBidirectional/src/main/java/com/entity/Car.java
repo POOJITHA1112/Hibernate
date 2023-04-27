ea9e43d5363cab0d7eaaa088020e5e56ba9f31a2
User <User@Poojitha>
2023-04-27 09:41:08 +0530

    Many to Many Bidirectional

---
+++ ManyToManyBidirectional/src/main/java/com/entity/Car.java
@@ -0,0 +1,70 @@
+package com.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Car {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long cid;
+	private String brand;
+	private String color;
+	private double price;
+	
+	@ManyToMany(cascade = CascadeType.ALL )
+	private List<Person>persons;
+	
+	public Car() {
+
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public long getCid() {
+		return cid;
+	}
+
+	public List<Person> getPersons() {
+		return persons;
+	}
+
+	public void setPersons(List<Person> persons) {
+		this.persons = persons;
+	}
+	
+	
+	
+
+}
---
+++ ManyToManyBidirectional/src/main/java/com/entity/Person.java
@@ -0,0 +1,83 @@
+package com.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Person {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String fName;
+	private String lName;
+	private String email;
+	
+	@ManyToMany(mappedBy = "persons",cascade = CascadeType.ALL)
+	private List<Car>cars;
+	
+	
+	public Person() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public String getfName() {
+		return fName;
+	}
+
+
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+
+
+	public String getlName() {
+		return lName;
+	}
+
+
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public long getId() {
+		return id;
+	}
+
+
+	public List<Car> getCars() {
+		return cars;
+	}
+
+
+	public void setCars(List<Car> cars) {
+		this.cars = cars;
+	}
+	
+	
+	
+	
+
+}
